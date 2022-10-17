cc41ffec1a87d54cee4e3ebf4a63ce810777f909
Kirill.Cheremisin <cheremisin_k@mail.ru>
2022-10-18 02:11:33 +0300

    log server in main

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -11,8 +11,14 @@ import (
 
 func main() {
 	h := handler.NewHandler()
+
 	r := chi.NewRouter()
+
 	r.Get("/hello", h.Hello)
+
+	log.Print("starting server")
 	err := http.ListenAndServe(":8080", r)
 	log.Fatal(err)
+
+	log.Print("shutting server down")
 }

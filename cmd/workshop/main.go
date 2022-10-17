4f125e9a9aecde04d9a82bedd1cc04a1fb9e0758
Kirill.Cheremisin <cheremisin_k@mail.ru>
2022-10-18 01:43:14 +0300

    add routing

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/go-chi/chi"
+
+	"workshop/internal/handler"
+)
+
+func main() {
+	h := handler.NewHandler()
+	r := chi.NewRouter()
+	r.Get("/hello", h.Hello)
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
 }
